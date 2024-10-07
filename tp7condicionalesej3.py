0e04f029163497aea1a971d88ba28d76f74d0605
felipeparis2009 <167116322+felipeparis2009@users.noreply.github.com>
2024-10-07 15:53:08 -0300

    a
    ejrcicios 8 de octubre

---
+++ tp7condicionalesej3.py
@@ -0,0 +1,5 @@
+a = str(input("decime la contraseña"))
+if a == "contraseña":
+    print("Bienvenido")
+if a != "contraseña":
+    print("contraseña incorecta")
---
+++ tp7condicionalesej4.py
@@ -0,0 +1,12 @@
+a = int(input("¿sos mayor? ¿estas trabajando?"))
+if a == "si":
+    print("paga el 100%")
+else:
+    a == "no"
+    print("paga el 50%")
+b = int(input("¿sos menor? ¿estas trabajando?"))
+if b == "si":
+    print("paga el 95%")
+else:
+    a == "no"
+    print("paga el 75%")
